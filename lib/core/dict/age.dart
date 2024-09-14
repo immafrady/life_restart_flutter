2100228e069c84c0bbe6972949150b0727872c75
Frady Wang <immafrady@gmail.com>
2024-09-14 15:10:02 +0800

    feat: add talent processor in Age

--- lib/core/dict/age.dart
+++ lib/core/dict/age.dart
@@ -8,6 +8,7 @@ class Age {
 
   Age.fromJson(JSONMap json) {
     age = convertToInt(json['age']);
+    // 处理event
     final List<dynamic> rawEvent = json['event'];
     for (var e in rawEvent) {
       if (e is int) {
@@ -18,6 +19,19 @@ class Age {
           1 => (int.parse(pair[0]), 1.0),
           _ => (int.parse(pair[0]), double.parse(pair[1])),
         });
+      }
+    }
+    // 处理talent
+    final dynamic rawTalent = json['talent'];
+    List<dynamic> listTalent;
+    if (rawTalent != null) {
+      if (rawTalent is String) {
+        listTalent = rawTalent.split(',');
+      } else {
+        listTalent = rawTalent as List<dynamic>;
+      }
+      for (var t in listTalent) {
+        talent.add(convertToInt(t));
       }
     }
   }
